4a02c1eda273d165ed2515288b99d3f31671a796
luisbmora <moraluis718@gmail.com>
2026-06-23 19:29:13 -0600

    Configure Entity Framework and SQL Server

---
+++ src/ToyStore.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToyStore.Domain.Entities;
+
+namespace ToyStore.Infrastructure.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
